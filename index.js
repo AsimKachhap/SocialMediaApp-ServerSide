b537847de7d67335ade1212a54764cff92062302
AsimKachhap <asim26k@gmail.com>
2022-07-24 03:31:23 +0530

    basic setup for backend

--- index.js
+++ index.js
@@ -2,3 +2,21 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({limit:"30mb", extended:"true"}));
+app.use(bodyParser.urlencoded({limit:"30mb", extended:"true"}));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://asim26k:Usha2305@cluster0.7rkmxkx.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,  useUnifiedTopology:true})
+.then(()=>app.listen(PORT,()=>console.log(`Server is running in port: ${PORT}`)))
+.catch((error)=>console.log(error.message));
+
+//mongoose.set('useFindAndModify',false);
