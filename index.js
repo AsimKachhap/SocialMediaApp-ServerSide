ffdd3e9d4980eb4bf35c6d2d3bdce71ece34466d
AsimKachhap <asim26k@gmail.com>
2022-08-02 07:28:44 +0530

    Now running on .env variables

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
 import postRoutes from './routes/posts.js'
 
 const app = express();
+dotenv.config();
 
 
 app.use(bodyParser.json({limit:"30mb", extended:"true"}));
@@ -15,7 +17,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://asim26k:Usha2305@cluster0.7rkmxkx.mongodb.net/?retryWrites=true&w=majority";
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,  useUnifiedTopology:true})
 .then(()=>app.listen(PORT,()=>console.log(`Server is running in port: ${PORT}`)))
